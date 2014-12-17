1051623302a8a3993066517489a52b742591109d
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-12-16 18:25:51 -0700

    added Const

---
+++ src/thx/math/Const.hx
@@ -0,0 +1,34 @@
+package thx.math;
+
+class Const {
+  public static inline var TWO_PI = 6.283185307179586477;
+
+/**
+Math constant PI.
+*/
+  public static inline var PI = 3.141592653589793238;
+
+/**
+Math constant PI/2.
+*/
+  public static inline var HALF_PI = 1.570796326794896619;
+
+/**
+Constant used to convert angle from radians to degrees.trace
+Equivalent to `180 / Math.PI`.
+*/
+  public static inline var TO_DEGREE = 57.29577951308232088;
+
+/**
+Constant used to convert degrees to radians.
+
+Equivalent to `Math.PI / 180`.
+*/
+  public static inline var TO_RADIAN = 0.01745329251994329577;
+
+  public static inline var LN10 = 2.302585092994046;
+
+  public static inline var E = 2.71828182845904523536;
+
+  public static inline var GOLDEN_RATIO = 1.6180339887498948482;
+}
