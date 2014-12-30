bb175c620724ea0087b32689182f6ae80620d327
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-12-30 12:35:36 -0700

    added documentation hooks

--- doc/ImportFormat.hx
+++
@@ -1,1 +0,0 @@
-import thx.format.;
---
+++ doc/ImportMath.hx
@@ -0,0 +1,6 @@
+import thx.math.Const;
+import thx.math.Equations;
+import thx.math.Integrate;
+import thx.math.random.NativeRandom;
+import thx.math.random.PseudoRandom;
+import thx.math.random.Random;
